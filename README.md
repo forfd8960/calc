dc75b2bf8bc487b436bdd10e925157135737a3c8
forfd8960 <forfd8960@gmail.com>
2024-09-04 09:22:00 +0800

    init keyboard

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# calc
+# Calculator App
+
 a calculator with rust and egui
+
+## init keyboard
+
+![alt text](init_keyboard.png)
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,15 @@ use keypad::Keypad;
 fn main() -> eframe::Result {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([750.0, 600.0])
+            .with_resizable(true)
+            .with_transparent(true),
         ..Default::default()
     };
 
     eframe::run_native(
-        "Custom Keypad App",
+        "Caculator App",
         options,
         Box::new(|cc| {
             // Use the dark theme
@@ -29,6 +32,7 @@ struct MyApp {
     name: String,
     age: u32,
     keypad: Keypad,
+    exp: String,
 }
 
 impl MyApp {}
@@ -36,33 +40,73 @@ impl MyApp {}
 impl Default for MyApp {
     fn default() -> Self {
         Self {
-            name: "Arthur".to_owned(),
+            name: "Alex".to_owned(),
             age: 42,
             keypad: Keypad::new(),
+            exp: String::new(),
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::Window::new("Custom Keypad")
+        egui::Window::new("Caculator")
             .default_pos([100.0, 100.0])
             .title_bar(true)
             .show(ctx, |ui| {
                 ui.horizontal(|ui| {
-                    ui.label("Your name: ");
-                    ui.text_edit_singleline(&mut self.name);
+                    ui.text_edit_singleline(&mut self.exp);
                 });
 
-                ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-                if ui.button("Increment").clicked() {
-                    self.age += 1;
-                }
+                // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+
+                ui.horizontal(|ui| {
+                    if ui.button("C").clicked() {
+                        self.exp.clear();
+                    }
+                    if ui.button("(").clicked() {
+                        self.exp.push_str("(");
+                    }
+                    if ui.button(")").clicked() {
+                        self.exp.push_str(")");
+                    }
+                });
+
+                ui.horizontal(|ui| {
+                    for i in 7..=10 {
+                        if ui.button(format!("{}", i)).clicked() {
+                            self.exp.push_str(&i.to_string());
+                        }
+                    }
+                });
+                ui.horizontal(|ui| {
+                    for i in 4..=7 {
+                        if ui.button(format!("{}", i)).clicked() {
+                            self.exp.push_str(&i.to_string());
+                        }
+                    }
+                });
+                ui.horizontal(|ui| {
+                    for i in 1..=4 {
+                        if ui.button(format!("{}", i)).clicked() {
+                            self.exp.push_str(&i.to_string());
+                        }
+                    }
+                });
+                ui.horizontal(|ui| {
+                    if ui.button("0").clicked() {
+                        self.exp.push_str("0");
+                    }
+                    if ui.button(format!(".")).clicked() {
+                        self.exp.push_str(".");
+                    }
+                });
 
                 ui.label(format!("Hello '{}', age {}", self.name, self.age));
+                ui.label(format!("exp: {}", self.exp));
             });
 
-        self.keypad.show(ctx);
+        // self.keypad.show(ctx);
     }
 
     fn raw_input_hook(&mut self, ctx: &egui::Context, raw_input: &mut egui::RawInput) {
